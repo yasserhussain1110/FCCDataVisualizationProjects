8f0c056522b6ba2131a7c8a1760a60eb83938648
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-08 18:32:48 +0530

    cleaned up

--- CamperLeaderBoard/app/index.js
+++ CamperLeaderBoard/app/index.js
@@ -12,38 +12,26 @@ class App extends Component {
     super(props);
 
     this.state = {currentSelection: 'past30', past30Campers: [], allTimeCampers: []};
-    //console.log(this.state);
 
     request
       .get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-      //.get('http://localhost:4440/recent')
       .end((err, res) => {
         this.setState({'past30Campers': res.body});
-        //console.log("past30 success");
-        //console.log(this.state);
       });
 
     request
       .get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-      //.get('http://localhost:4440/alltime')
       .end((err, res) => {
         this.setState({'allTimeCampers': res.body});
-        //console.log("alltime success");
-        //console.log(this.state);
       });
   }
 
   render() {
     if (this.state.currentSelection === 'past30') {
-      //console.log("In here");
       var campers = this.state.past30Campers;
-      //console.log(this.state);
     } else if (this.state.currentSelection === 'alltime') {
       var campers = this.state.allTimeCampers;
     }
-
-    //console.log(this.state.currentSelection);
-    //console.log(campers);
 
     return (
       <div className="container">
