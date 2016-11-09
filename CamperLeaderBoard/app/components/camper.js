372a6fe77d77097dd18d59b71cae34750ec8798e
Yasser Hussain <yasserhussain1110@gmail.com>
2016-11-09 08:38:30 +0530

    cleanup

--- CamperLeaderBoard/app/components/camper.js
+++ CamperLeaderBoard/app/components/camper.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default ({index, camper}) => {
   return (
     <tr>
-      <td>{index+1}</td>
-      <td className="camper-name"><a href={"https://www.freecodecamp.com/"+camper.username}>
-        <img className="userimg" src={camper.img} />{camper.username}
-        </a>
+      <td>{index + 1}</td>
+      <td className="camper-name"><a href={"https://www.freecodecamp.com/" + camper.username}>
+        <img className="userimg" src={camper.img}/>{camper.username}
+      </a>
       </td>
       <td className="camper-recent">{camper.recent}</td>
       <td className="camper-alltime">{camper.alltime}</td>
--- CamperLeaderBoard/codepen/app.js
+++ CamperLeaderBoard/codepen/app.js
@@ -3,9 +3,9 @@ var request = superagent;
 var Camper = ({index, camper}) => {
   return (
     <tr>
-      <td>{index+1}</td>
-      <td className="camper-name"><a href={"https://www.freecodecamp.com/"+camper.username}>
-        <img className="userimg" src={camper.img} />{camper.username}
+      <td>{index + 1}</td>
+      <td className="camper-name"><a href={"https://www.freecodecamp.com/" + camper.username}>
+        <img className="userimg" src={camper.img}/>{camper.username}
       </a>
       </td>
       <td className="camper-recent">{camper.recent}</td>
