074de5cb555440e757f5d43673a4179d848b988f
Yasser Hussain <yasserhussain1110@gmail.com>
2016-12-27 19:10:00 +0530

    Refactored Code to aid in adding more components.

--- DungeonCrawler/app/components/App.js
+++ DungeonCrawler/app/components/App.js
@@ -1,130 +1,24 @@
 import React, {Component} from 'react';
-import Board from './board';
-import get_dungeon from '../gameobjects/dungeon';
-import get_player from '../gameobjects/player';
-import get_enemies from '../gameobjects/enemies';
-import get_weapons from '../gameobjects/weapons';
-import get_healths from '../gameobjects/healths';
-import get_transporter from '../gameobjects/transporter';
-import get_boss from '../gameobjects/boss';
-import LightScreen from '../screens/LightScreen';
-import move from '../actionhandlers/move';
-import _ from 'lodash';
+import Board from './Board';
 
-const game_args = {
-  total_number_of_rows: 102,
-  total_number_of_columns: 100,
-  number_of_rooms: 20,
-  max_width: 20,
-  min_width: 4,
-  max_height: 20,
-  min_height: 4
+const App = (props) => {
+  return (
+    <div id="app">
+      <Board game_args={props.game_args}/>
+    </div>
+  );
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    /*
-     All possible attributes are shown below for documentation purposes.
-     The state object will be initialized later.
-     */
-
-    this.handleKeyPress = _.throttle(this.handleKeyPress.bind(this), 50);
-
-    this.state = {
-      dungeon: null,
-      dungeonLevel: 1,
-      player: null,
-      enemies: null,
-      boss: null,
-      weapons: null,
-      healths: null,
-      transporter: null,
-      gameOver: false,
-      playerWon: false,
-      screen: new LightScreen(game_args.total_number_of_rows, game_args.total_number_of_columns)
-    };
-  }
-
-  readyGame(dungeonLevel) {
-    var number_of_enemies = 10;
-    var number_of_healths = 6;
-    var gameObjects = [];
-
-    const dungeon = get_dungeon(game_args);
-
-    const player = get_player(dungeon, dungeonLevel, this.state.player);
-    gameObjects.push(player);
-
-    const enemies = get_enemies(dungeon, dungeonLevel, gameObjects, number_of_enemies);
-    Array.prototype.push.apply(gameObjects, enemies);
-
-    var boss, transporter = null;
-    if (this.isLastLevel()) {
-      boss = get_boss();
-      gameObjects.push(boss);
-    } else {
-      transporter = get_transporter(dungeon, gameObjects);
-      gameObjects.push(transporter);
-    }
-
-    const weapons = get_weapons(dungeon, dungeonLevel, gameObjects);
-    Array.prototype.push.apply(gameObjects, weapons);
-
-    const healths = get_healths(dungeon, dungeonLevel, gameObjects, number_of_healths);
-    Array.prototype.push.apply(gameObjects, healths);
-
-    return {
-      dungeon,
-      dungeonLevel,
-      player,
-      enemies,
-      boss,
-      weapons,
-      healths,
-      transporter
-    }
-  }
-
-  componentWillMount() {
-    const gameState = this.readyGame(1);
-    this.setState(gameState);
-  }
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyPress);
-  }
-
-  isLastLevel() {
-    return this.state.dungeonLevel === 4;
-  }
-
-  isKeyPressEventConsumable(e) {
-    return e.key === "ArrowRight" ||
-      e.key === "ArrowLeft" ||
-      e.key === "ArrowUp" ||
-      e.key === "ArrowDown";
-  }
-
-  handleKeyPress(e) {
-    if(this.isKeyPressEventConsumable(e)) {
-      this.setState(move(e.key, this.state));
-      e.preventDefault();
-    }
-  }
-
-  render() {
-    return (
-      <div id="app">
-        <Board
-          {...this.state}
-          total_number_of_rows={game_args.total_number_of_rows}
-          total_number_of_columns={game_args.total_number_of_columns}
-        />
-      </div>
-    );
-  }
-}
-
 export default App;
+
+App.defaultProps = {
+  game_args: {
+    total_number_of_rows: 102,
+    total_number_of_columns: 100,
+    number_of_rooms: 20,
+    max_width: 20,
+    min_width: 4,
+    max_height: 20,
+    min_height: 4
+  }
+};
---
+++ DungeonCrawler/app/components/Board.js
@@ -0,0 +1,146 @@
+import React, {Component} from 'react';
+import get_dungeon from '../gameobjects/dungeon';
+import get_player from '../gameobjects/player';
+import get_enemies from '../gameobjects/enemies';
+import get_weapons from '../gameobjects/weapons';
+import get_healths from '../gameobjects/healths';
+import get_transporter from '../gameobjects/transporter';
+import get_boss from '../gameobjects/boss';
+import LightScreen from '../screens/LightScreen';
+import move from '../actionhandlers/move';
+import _ from 'lodash';
+
+class Board extends Component {
+
+  constructor(props) {
+    super(props);
+
+    /*
+     All possible attributes are shown below for documentation purposes.
+     The state object will be initialized later.
+     */
+
+    this.handleKeyPress = _.throttle(this.handleKeyPress.bind(this), 50);
+
+    this.state = {
+      dungeon: null,
+      dungeonLevel: 1,
+      player: null,
+      enemies: null,
+      boss: null,
+      weapons: null,
+      healths: null,
+      transporter: null,
+      gameOver: false,
+      playerWon: false,
+      screen: new LightScreen(props.game_args.total_number_of_rows, props.game_args.total_number_of_columns)
+    };
+  }
+
+
+  readyGame(dungeonLevel) {
+    var number_of_enemies = 10;
+    var number_of_healths = 6;
+    var gameObjects = [];
+
+    var game_args = this.props.game_args;
+
+    const dungeon = get_dungeon(game_args);
+
+    const player = get_player(dungeon, dungeonLevel, this.state.player);
+    gameObjects.push(player);
+
+    const enemies = get_enemies(dungeon, dungeonLevel, gameObjects, number_of_enemies);
+    Array.prototype.push.apply(gameObjects, enemies);
+
+    var boss, transporter = null;
+    if (this.isLastLevel()) {
+      boss = get_boss();
+      gameObjects.push(boss);
+    } else {
+      transporter = get_transporter(dungeon, gameObjects);
+      gameObjects.push(transporter);
+    }
+
+    const weapons = get_weapons(dungeon, dungeonLevel, gameObjects);
+    Array.prototype.push.apply(gameObjects, weapons);
+
+    const healths = get_healths(dungeon, dungeonLevel, gameObjects, number_of_healths);
+    Array.prototype.push.apply(gameObjects, healths);
+
+    return {
+      dungeon,
+      dungeonLevel,
+      player,
+      enemies,
+      boss,
+      weapons,
+      healths,
+      transporter
+    }
+  }
+
+  componentWillMount() {
+    const gameState = this.readyGame(1);
+    this.setState(gameState);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  isLastLevel() {
+    return this.state.dungeonLevel === 4;
+  }
+
+  isKeyPressEventConsumable(e) {
+    return e.key === "ArrowRight" ||
+      e.key === "ArrowLeft" ||
+      e.key === "ArrowUp" ||
+      e.key === "ArrowDown";
+  }
+
+  handleKeyPress(e) {
+    if (this.isKeyPressEventConsumable(e)) {
+      this.setState(move(e.key, this.state));
+      e.preventDefault();
+    }
+  }
+
+  render() {
+    return (
+      <BoardSlave {...this.state} />
+    );
+
+  }
+}
+
+const BoardSlave = ({screen, dungeon, player, enemies, weapons, healths, boss, transporter}) => {
+
+  screen.initBoardWithPlayerPosition(player.position);
+
+  screen.placeRooms(dungeon.rooms);
+
+  screen.placeConnectionCells(dungeon.connection_cells);
+
+  screen.placePlayer(player);
+
+  screen.placeEnemies(enemies);
+
+  screen.placeWeapons(weapons);
+
+  screen.placeHealths(healths);
+
+
+  if (boss) {
+    screen.placeBoss(boss);
+  }
+
+  if (transporter) {
+    screen.placeTransporter(transporter);
+  }
+
+  return <div className="board">{screen.getRenderableBoard()}</div>;
+};
+
+export default Board;
